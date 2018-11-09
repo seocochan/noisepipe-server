d858e1e052268c406a7bbed9b61556f30bbcfe90
seoco <seokho.dev@gmail.com>
2018-11-09 18:32:22 +0900

    Add status endpoint for aws lb

--- src/main/java/com/noisepipe/server/config/SecurityConfig.java
+++ src/main/java/com/noisepipe/server/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .permitAll()
             .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
             .permitAll()
-            .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+            .antMatchers(HttpMethod.GET, "/api/status/**", "/api/polls/**", "/api/users/**")
             .permitAll()
             .anyRequest()
             .authenticated();
