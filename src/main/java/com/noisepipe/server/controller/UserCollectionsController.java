1151a027b457debecfefc9ca3e1b77a138caf8b9
seoco <seokho.dev@gmail.com>
2018-11-18 13:05:40 +0900

    Apply default values to pageable params

--- src/main/java/com/noisepipe/server/controller/UserCollectionsController.java
+++ src/main/java/com/noisepipe/server/controller/UserCollectionsController.java
@@ -3,6 +3,7 @@ package com.noisepipe.server.controller;
 import com.noisepipe.server.payload.CollectionResponse;
 import com.noisepipe.server.payload.PagedResponse;
 import com.noisepipe.server.service.CollectionService;
+import com.noisepipe.server.utils.AppConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,9 +15,10 @@ public class UserCollectionsController {
   private final CollectionService collectionService;
 
   @GetMapping
-  public PagedResponse<CollectionResponse> getCollectionsByUsername(@PathVariable String username,
-                                                                    @RequestParam int page,
-                                                                    @RequestParam int size) {
+  public PagedResponse<CollectionResponse> getCollectionsByUsername(
+          @PathVariable String username,
+          @RequestParam(defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+          @RequestParam(defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
     return collectionService.getCollectionsByUsername(username, page, size);
   }
 }
