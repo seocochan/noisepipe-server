44c033e6c5a787d8014a3b7e343b8fcc1323d763
seoco <seokho.dev@gmail.com>
2019-02-04 17:16:59 +0900

    Remove unique constraints of item entity

--- src/main/java/com/noisepipe/server/model/Item.java
+++ src/main/java/com/noisepipe/server/model/Item.java
@@ -14,11 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "items", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {
-                "collection_id", "position"
-        })
-})
+@Table(name = "items")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
